57ecf2026b475b4260e9fe452f71f5fbe30f74dc
jmeirink <84925553+jmeirink@users.noreply.github.com>
2021-11-12 10:47:31 -0600

    Uploaded project files to repository

---
+++ index.js
@@ -0,0 +1,17 @@
+//jshint esversion:6
+
+
+// Superheroes
+const superheroes = require('superheroes');
+
+const mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+
+
+// Supervillains
+const supervillains = require('supervillains');
+
+const mySuperVillainName = supervillains.random();
+
+console.log(mySuperVillainName);
